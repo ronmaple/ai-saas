c5bb3c88e25ef7527744d51a0e1a1f3b3ccf6ff8
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-11-15 10:59:54 -0800

    base setup

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  // return <p className="text-6xl text-green-500">Hello AI SaaS</p>
+  return <Button variant="destructive">Click me</Button>
+}
